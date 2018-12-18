6fb215072bd033ffc085543982bf1a7bf0498e1e
kriszbosh <harmaskrisz@gmail.com>
2018-12-18 15:47:06 +0100

    no message

--- angular-kepzes/src/app/app.module.ts
+++ angular-kepzes/src/app/app.module.ts
@@ -6,11 +6,27 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FilesComponent } from './files/files.component';
+import { FileItemComponent } from './files/file-item/file-item.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MembersComponent } from './members/members.component';
+import { UserComponent } from './members/user/user.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProjectComponent } from './project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FilesComponent,
+    FileItemComponent,
+    SidebarComponent,
+    DashboardComponent,
+    MembersComponent,
+    UserComponent,
+    ProfileComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
